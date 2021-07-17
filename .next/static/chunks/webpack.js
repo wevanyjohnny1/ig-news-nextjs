9e2ddf7c2f9a53a3d7b65e377385e3dc4164cebe
johnny <johnnywevany@outlook.com>
2021-07-17 14:32:26 -0300

    adding

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b9744dc4161c28a4f376"; }
+/******/ 		__webpack_require__.h = function() { return "f8e3eabdb3b51d334bea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -22,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'checkout.subscription.updated',
+  'checkout.subscription.deleted'
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -42,7 +45,37 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Evento recebido', event)
+      try {
+        switch (type) {
+          case 'checkout.subscription.updated':
+          case 'checkout.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          case 'checkout.session.completed':
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            )
+
+            break;
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch {
+        return res.json({ error: 'Webhook handler failed.' })
+      }
     }
 
     res.json({ received: true })
